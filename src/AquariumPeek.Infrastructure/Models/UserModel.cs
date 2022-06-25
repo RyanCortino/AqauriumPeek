32aa468dc722ab2e3807fa1d4aeda25c3080b68b
Ryan Cortino <ryancortino@outlook.com>
2022-06-25 11:10:08 -0500

    Adds Model and DTO for Users

---
+++ src/AquariumPeek.Infrastructure/Models/UserModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AquariumPeek.Infrastructure.Models
+{
+    public class UserModel
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ src/AquariumPeek.Infrastructure/Persistence/UserData.cs
@@ -0,0 +1,40 @@
+﻿using AquariumPeek.Infrastructure.DataAccess;
+using AquariumPeek.Infrastructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AquariumPeek.Infrastructure.Persistence
+{
+    public class UserData
+    {
+        private readonly ISqlDataAccess _db;
+
+        public UserData(ISqlDataAccess db)
+        {
+            _db = db;
+        }
+
+        public Task<IEnumerable<UserModel>> GetUsers()
+            => _db.LoadData<UserModel, dynamic>("dbo.spUser_GetAll", new { });
+
+        public async Task<UserModel> GetUser(Guid id)
+            => (await LoadUser(id)).FirstOrDefault();
+
+        public Task InserUser(UserModel user)
+            => _db.SaveData("dbo.spUser_Insert", new { user.Id, user.Name });
+
+        public Task UpdateUser(UserModel user)
+            => _db.SaveData("dbo.spUser_Update", user);
+
+        public Task DeleteUser(Guid guid)
+            => _db.SaveData("dbo.spUser_Delete", new { Id = guid });
+
+        private async Task<IEnumerable<UserModel>> LoadUser(Guid id)
+        {
+            return await _db.LoadData<UserModel, dynamic>("dbo.spUser_Get", new UserModel { Id = id });
+        }
+    }
+}
